e7f1316f079a5dbc4af6900e200dcdd4c420ad5d
geansru <d@geans.ru>
2016-04-13 16:56:53 +0400

    initial commit

---
+++ Showtime/Data/MovieService.swift
@@ -0,0 +1,89 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import JavaScriptCore
+
+let movieUrl = "https://itunes.apple.com/us/rss/topmovies/limit=50/json"
+
+class MovieService {
+    lazy var context: JSContext? = {
+        let context = JSContext()
+        guard let
+            commonJSPath = NSBundle.mainBundle().pathForResource("common", ofType: "js"),
+            additionsJSPath = NSBundle.mainBundle().pathForResource("additions", ofType: "js")
+        else {
+            print("Unable to read resource file.")
+            return nil
+        }
+        do {
+            let common = try String(contentsOfFile: commonJSPath, encoding: NSUTF8StringEncoding)
+            let addition = try String(contentsOfFile: additionsJSPath, encoding: NSUTF8StringEncoding)
+            
+            context.setObject(Movie.self, forKeyedSubscript: "Movie")
+            
+            context.evaluateScript(common)
+            context.evaluateScript(addition)
+        } catch let e as NSError {
+            print("Error while processing file: \(e.localizedDescription)")
+        }
+        
+        return context
+    }()
+    
+    func loadMoviesWithLimit(limit: Double, onComplete complete:[Movie] -> ()) {
+        guard let url = NSURL(string: movieUrl) else {
+            print("Invalid url: \(movieUrl)")
+            return
+        }
+
+        NSURLSession.sharedSession().dataTaskWithURL(url) { [unowned self] data, _, _ in
+            guard let data = data, jsonString = String(data: data, encoding: NSUTF8StringEncoding) else {
+                print("Error while parsing the response data")
+                return
+            }
+            
+            let movies = self.parseResponse(jsonString, withLimit: limit)
+            complete(movies)
+        }.resume()
+    }
+
+    func parseResponse(response: String, withLimit limit: Double) -> [Movie] {
+        guard let context = context else {
+            print("JSContext not found!")
+            return []
+        }
+        let parseFunction = context.objectForKeyedSubscript("parseJson")
+        let parsed = parseFunction.callWithArguments([response]).toArray()
+        
+        let filteredFunction = context.objectForKeyedSubscript("filterByLimit")
+        let filtered = filteredFunction.callWithArguments([parsed, limit]).toArray()
+        
+        let mapFunction = context.objectForKeyedSubscript("mapToNative")
+        guard let unwrappedFiltered = filtered,
+            movies = mapFunction.callWithArguments([unwrappedFiltered]).toArray() as? [Movie] else {
+                return []
+        }
+        
+        return movies
+    }
+}
---
+++ Showtime/Model/Movie.swift
@@ -0,0 +1,60 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import Foundation
+import JavaScriptCore
+
+@objc protocol MovieJSExports: JSExport {
+    var title: String { get set }
+    var price: String { get set }
+    var imageUrl: String { get set }
+    
+    static func movieWithTitle(title: String, price: String, imageUrl: String) -> Movie
+}
+
+class Movie: NSObject {
+
+    dynamic var title: String
+    dynamic var price: String
+    dynamic var imageUrl: String
+
+    init(title: String, price: String, imageUrl: String) {
+        self.title = title
+        self.price = price
+        self.imageUrl = imageUrl
+    }
+    
+    convenience init(fromDictionary dictionary: [String: String]) {
+        guard let title = dictionary["title"], price = dictionary["price"], imageUrl = dictionary["imageUrl"] else { fatalError() }
+        self.init(title: title, price: price, imageUrl: imageUrl)
+    }
+    
+    static let movieBuilder: @convention(block) [[String:String]] -> [Movie] = {
+        $0.map { Movie(fromDictionary: $0) }
+    }
+}
+
+extension Movie: MovieJSExports {
+    class func movieWithTitle(title: String, price: String, imageUrl: String) -> Movie {
+        return Movie(title: title, price: price, imageUrl: imageUrl)
+    }
+}
